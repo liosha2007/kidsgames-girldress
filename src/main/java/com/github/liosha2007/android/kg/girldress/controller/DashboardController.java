0a26ba4257b1f3229a37c484727e0757c55fbe12
aleksey.permyakov <liosha.89@gmail.com>
2014-06-30 20:39:28 +0300

    Added music

--- src/main/java/com/github/liosha2007/android/kg/girldress/controller/DashboardController.java
+++ src/main/java/com/github/liosha2007/android/kg/girldress/controller/DashboardController.java
@@ -1,12 +1,12 @@
 package com.github.liosha2007.android.kg.girldress.controller;
 
+import android.media.MediaPlayer;
+
 import com.github.liosha2007.android.kg.girldress.R;
 import com.github.liosha2007.android.kg.girldress.common.ComponentType;
 import com.github.liosha2007.android.kg.girldress.view.DashboardView;
 import com.github.liosha2007.android.library.activity.controller.BaseActivityController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,9 +14,12 @@ import java.util.TreeMap;
  * @autored by liosha on 29.06.2014.
  */
 public class DashboardController extends BaseActivityController<DashboardView> {
-    protected Map<ComponentType, Integer> componentsToDraw = new TreeMap<ComponentType, Integer>(){{
+    protected static float PLAYER_VOLUME = 0.08f;
+    protected MediaPlayer mPlayer;
+    protected Map<ComponentType, Integer> componentsToDraw = new TreeMap<ComponentType, Integer>() {{
         put(ComponentType.HEIR, R.drawable.im_volosy_1);
     }};
+    protected int playerPosition;
 
     public DashboardController() {
         super(new DashboardView());
@@ -28,6 +31,12 @@ public class DashboardController extends BaseActivityController<DashboardView> {
 
         view.drawState(componentsToDraw);
         onGroupClicked(ComponentType.DRESS);
+        mPlayer = MediaPlayer.create(this, R.raw.mp_anime_1);
+        if (mPlayer != null) {
+            mPlayer.setVolume(PLAYER_VOLUME, PLAYER_VOLUME);
+            mPlayer.setLooping(true);
+            mPlayer.start();
+        }
     }
 
     public void onGroupClicked(ComponentType componentType) {
@@ -42,5 +51,43 @@ public class DashboardController extends BaseActivityController<DashboardView> {
     public void onComponentClicked(ComponentType componentType, int id) {
         componentsToDraw.put(componentType, id);
         view.drawState(componentsToDraw);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mPlayer != null && mPlayer.isPlaying()) {
+            mPlayer.pause();
+            playerPosition = mPlayer.getCurrentPosition();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mPlayer != null) {
+            mPlayer.seekTo(playerPosition);
+            mPlayer.start();
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
+        super.onDestroy();
     }
 }
